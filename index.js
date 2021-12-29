4357650eac645a749563583271e3c0970a34838c
SouadTorfi <souad.torfi99@gmail.com>
2021-12-29 19:46:03 +0200

    Step 4

--- index.js
+++ index.js
@@ -10,15 +10,43 @@ app.get('/', (req, res) => {
 })
 
 app.get('/test', (req, res) => {
-  res.send({status:200, message:"ok"})
+  res.send({ status: 200, message: "ok" })
 })
 
 let date = new Date();
 
 app.get("/time", (req, res) => {
-  res.send({status:200,message: date.getHours() + ":" + date.getSeconds()
+  res.send({
+    status: 200, message: date.getHours() + ":" + date.getSeconds()
   });
 });
+
+app.get('/hello/:id', (req, res) => {
+
+  res.send({ status: 200, message: "Hello " + req.params.id })
+
+})
+app.get('/hello/', (req, res) => {
+
+  res.send({ status: 200, message: "Hello " + "Unknow" })
+
+})
+
+
+
+app.get(['/search','/search/:data'], (req, res) => {
+  //not null
+  if(req.params.data!=null){
+    res.send({status:200, message:"ok " +req.params.data})
+
+  }else{
+    res.send({status:500,error:true, message:"you have to provide a search" })
+    //to set status https:500 in Network 
+    //res.sendStatus(500 ).send("any things you want")
+    
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
